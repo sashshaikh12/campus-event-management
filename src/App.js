d8b0fd310cecfb4268086b509d27ab23770670f8
fierychicken65 <fadiltutu@gmail.com>
2024-11-04 18:05:45 +0530

    going with the vibes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>king</h1>
       </header>
     </div>
   );
-}
 
-export default App;
+}
