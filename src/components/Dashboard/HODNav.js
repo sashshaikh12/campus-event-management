81ddba933a7ca305eb286785850112569c44f8f7
fierychicken65 <fadiltutu@gmail.com>
2024-11-12 18:46:36 +0530

    added notification button

--- src/components/Dashboard/HODNav.js
+++ src/components/Dashboard/HODNav.js
@@ -1,13 +1,17 @@
 import React,{useState} from "react";
 import { Link } from "react-router-dom";
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Import Font Awesome
 
 
 export default function HODNav(){
     const [showProfileOptions, setShowProfileOptions] = useState(false);
+    const [showNotifications, setShowNotifications] = useState(false);
     const toggleProfileOptions = () => {
         setShowProfileOptions(!showProfileOptions);
     };
-
+    const toggleNotifications = () => {
+        setShowNotifications(!showNotifications);
+    };
     return(
         <div className="studentNav">
             <img className = "peslogo" src="/newpeslogo.png" alt="pes university logo" />
@@ -16,12 +20,23 @@ export default function HODNav(){
                     <button>Faculty</button>
                     <button>Events</button>
             </div>
-            <button className="ProfileButton" onClick={toggleProfileOptions}>Profile</button>
+            <div className="notificationContainer">
+                <button className="NotificationButton" onClick={toggleNotifications}> 
+                    <i class="fa-solid fa-bell"></i>  
+                </button>
+                {showNotifications && (
+                    <div className="notifications">
+                        <p>No new notifications</p>
+                    </div>
+                )}
+            </div>
             <div className={`profileOptions ${showProfileOptions ? 'show' : ''}`}>
                 <button>View Profile</button>
                 <div className="spacer"></div>
                 <button className="logout">Logout</button>
             </div>
+            <button className="ProfileButton" onClick={toggleProfileOptions}>Profile</button>
+            
         </div>
     );
 }
