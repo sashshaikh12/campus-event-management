4ae911e02a92abccad5fc3809fcf6db7a0f4c9b6
fierychicken65 <fadiltutu@gmail.com>
2024-11-14 19:33:36 +0530

    made changes to eventinfo

--- src/components/Dashboard/HOD/eventinfo.js
+++ src/components/Dashboard/HOD/eventinfo.js
@@ -49,7 +49,23 @@ export default function EventInfo(){
                 ))}
             </div>
             <form className="approvedEvents">
-
+            <h1 className="form-title">APPROVED</h1>
+                {events.map(event => (
+                        <div className="eventsform" key={event.Event_ID}>
+                            <img src={event.URL} alt="event-img" className="event-img" />
+                            <div className="eventformdetails">
+                                <p className="event-name"><span>Event:</span> {event.Event_name}</p>
+                                <p className="description"><span>Description:</span> {event.Event_description}</p>
+                                <p className="event-date"><span>Date:</span> {event.Event_date}</p>
+                                <p className="event-location"><span>Location:</span> {event.Location}</p>
+                                <p className="event-time"><span>Time:</span> {event.Event_time}</p>
+                                <p className="event-room"><span>Room:</span> {event.Room_name}</p>
+                            </div>
+                                <div className="acceptreject">
+                            <button className="reject-button"><i className="fa-solid fa-xmark"/></button>
+                        </div>
+                    </div>
+                    ))}
             </form>
         </div>
     )
